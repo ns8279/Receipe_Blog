84fe98a3265b98ae725229f08f75866363cdd2a3
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-06-21 15:02:25 -0700

    created all routes for data

---
+++ controllers/api/ingredient-routes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const { Recipe, User, Comment, Category, Ingredient } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Ingredient.findAll({
+            attributes: [
+                'id',
+                'ingredient_name',
+                'quantity',
+                'created_at'
+            ],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Recipe,
+                    attributes: ['recipe_id']
+                }
+            ]
+        })
+        .then(dbIngredientData => res.json(dbIngredientData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+router.get('/:id', (req, res) => {
+    Ingredient.findAll({
+            where: {
+                recipe_id: req.params.recipe_id
+            }
+        })
+        .then(dbIngredientData => res.json(dbIngredientData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+router.get('/', withAuth, (req, res) => {
+    if (req.session) {
+        Ingredient.create({
+                recipe_id: req.body.recipe_id,
+                ingredient_name: req.body.ingredient_name,
+                quantity: req.body.quantity,
+                user_id: req.session.user_id
+            })
+            .then(dbIngredientData => res.json(dbIngredientData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    Ingredient.update({
+            ingredient_name: req.body.ingredient_name,
+            quantity: req.body.quantity
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbIngredientData => {
+            if (!dbIngredientData) {
+                res.status(404).json({ message: 'There was no ingredient found with this id' });
+                return;
+            }
+            res.json(dbIngredientData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Ingredient.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbIngredientData => {
+            if (!dbIngredientData) {
+                res.status(404).json({ message: 'There was no ingredient found with this id' });
+                return;
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,134 @@
+const router = require('express').Router();
+const { Recipe, User, Comment, Ingredient, Category } = require('../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Recipe.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'title',
+                'recipe_name',
+                'prep_time',
+                'cook_time',
+                'recipte_method',
+                'pic',
+                'created_at'
+            ],
+            include: [{
+                    model: Ingredient,
+                    attributes: [
+                        'id',
+                        'recipe_id',
+                        'ingredient_name',
+                        'quantity'
+                    ]
+                },
+                {
+                    model: Category,
+                    attributes: [
+                        'id',
+                        'Category_name'
+                    ]
+                },
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_text',
+                        'user_id',
+                        'recipe_id'
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbRecipeData => {
+            const recipes = dbRecipeData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { recipes, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Recipe.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'title',
+                'recipe_name',
+                'prep_time',
+                'cook_time',
+                'recipte_method',
+                'pic',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Ingredient,
+                    attributes: [
+                        'id',
+                        'recipe_id',
+                        'ingredient_name',
+                        'quantity'
+                    ]
+                },
+                {
+                    model: Category,
+                    attributes: [
+                        'id',
+                        'Category_name'
+                    ]
+                },
+                {
+                    model: Comment,
+                    attributes: [
+                        'id',
+                        'comment_text',
+                        'user_id',
+                        'recipe_id'
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+            ]
+        })
+        .then(dbRecipeData => {
+            if (!dbRecipeData) {
+                res.status(404).json({ message: 'There was no recipe found with this id' });
+                return;
+            }
+            const recipe = dbRecipeData.get({ plain: true });
+            res.render('edit-recipe', { post, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/new', (req, res) => {
+    res.render('new-recipe');
+});
+
+module.exports = router;
